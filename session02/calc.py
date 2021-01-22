86d8555c1e6cd02dcd78c6664d5a80878e5a7b12
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-01-22 10:23:36 +0700

    exercise02

---
+++ session02/calc.py
@@ -0,0 +1,11 @@
+#2.1
+print((42*60)+42)
+#2.2
+print(10/1.61)
+#2.3
+#PACE:second/mile
+print((42*60+42)/(10/1.61))
+#PACE:minute/mile
+print(((42*60+42)/(10/1.61)/60))
+#SPEED:miles/hour
+print((10/1.61)/(42.7/60))
