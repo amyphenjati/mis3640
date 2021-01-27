b7fed660b83bf7a5d48a83428083126eed5f2d5f
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-01-27 11:23:51 +0700

    Update calc_2.py

--- session03/calc_2.py
+++ session03/calc_2.py
@@ -3,17 +3,12 @@
 # 2.1
 pi = 3.1415926535897932
 r = 5.0
-print(4.0 / 3.0 * pi * r ** 3)
+v = 4.0 / 3.0 * pi * r ** 3
+print(f'Volume of a sphere with radius=5 is{v:.2f}')
 
 # 2.2
-cover_price = 24.95
-discounted_price = cover_price * 0.6
-number_of_books = int(input("How many books in the wholesale order? "))
-shipping = 3 + (0.75 * (number_of_books - 1))
-wholesale_cost = discounted_price * number_of_books + shipping
-print(
-    "Total wholesale cost of", number_of_books, "books is $", round(wholesale_cost, 2)
-)
+wholesale_cost = (24.95*0.60)*60+3+0.75*(60-1)
+print(f'Total wholesale cost of 60 books is $ {wholesale_cost:.2f}')
 
 # 2.3
 start_time_hour = 6 + (52 / 60)
@@ -24,7 +19,8 @@ total_run_time_hour = 2 * easy_pace_hour + 3 * tempo_pace_hour
 breakfast_hour = start_time_hour + total_run_time_hour
 breakfast_min = (breakfast_hour - int(breakfast_hour)) * 60
 
-print("breakfast at", int(breakfast_hour), ":", int(breakfast_min))
+print(f'breakfast at {int(breakfast_hour)}:{int(breakfast_min)}')
 
 # 2.4
-print(round(((89 - 82) / 82) * 100, 1), "%")
+perc_change = ((89 - 82) / 82) * 100
+print(f'{perc_change:.1f}%')
