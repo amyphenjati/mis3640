180e426d22b193a688f61172963fe7d6974e3b72
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-02-10 09:11:05 +0700

    Update quiz0.py

---
+++ quiz/quiz0.py
@@ -0,0 +1,23 @@
+"""
+Question 1:
+Write a function that prompts the user for a weight on earth and prints the equivalent weight on the moon (16.5%)
+Weight on Moon = weight on Earth * 0.165
+Notice:
+1. Please write pseudo-code before coding the function.
+2. Your function should include docstring.
+3. Write your own test code, i.e. call the function.
+"""
+
+
+
+def calculate_weight_on_moon():
+    """prompts user to input values for weight on earth, converts them to
+    weight on moon."""
+    weight_on_earth = (input("Enter your weight (kg): "))
+
+    weight_on_moon = weight_on_earth * 0.165
+
+    return weight_on_moon
+
+print("Your weight on the Moon is: " calculate_weight_on_moon())
+
