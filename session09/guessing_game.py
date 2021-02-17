7f7e2f1ab5b2a14519a62b24675b7d1967ea6920
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-02-17 16:56:47 +0700

    Update guessing_game.py

--- session09/guessing_game.py
+++ session09/guessing_game.py
@@ -6,14 +6,14 @@ def number_guessing_game():
     guessesTaken = 0  # create variable to store number of guesses, starting from 0
 
     print("Hello! What is your name?")  # input name of player
-    myName = input()
+    myName = input(">>")
 
     number = random.randint(1, 20)  # generate a random number between 1-20
     print(f"Well, {myName}, I am thinking of a number between 1 and 20.")
 
     while guessesTaken < 6:  # if less than 6 guesses, repeat the following steps
         print("Take a guess.")
-        guess = int(input())
+        guess = int(input(">>"))
 
         guessesTaken = guessesTaken + 1  # count number of guesses
 
@@ -21,19 +21,15 @@ def number_guessing_game():
         if guess < number:
             print("Your guess is too low.")
 
-        if guess > number:
+        elif guess > number:
             print("Your guess is too high.")
 
-        # if guess correctly
-        if guess == number:
+        # output if guess correctly
+        else:
+            print(f"Good job, {myName}! You guessed my number in {guessesTaken} guesses!")
             break
 
-    if guess == number:  # output if guess correctly
-        str(guessesTaken)  # convert to string
-        print(f"Good job, {myName}! You guessed my number in {guessesTaken} guesses!")
-
-    if guess != number:  # if exceed 6  guesses
-        str(number)  # convert to string
+    if guess != number:  # if exceed 6 guesses
         print(f"Nope. The number I was thinking of was {number}")
 
 
