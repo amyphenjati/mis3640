087fb22a42b0a8fb98a872b03621b1e8a151cc85
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-03-10 08:09:00 +0700

    Update json_example.py

--- session13/json_example.py
+++ session13/json_example.py
@@ -1,14 +1,15 @@
 import urllib.request
 import json
+import
 
-APIKEY = '22499eddd60c8b6d76550fa01c151bcc'
-city = 'Wellesley'
-country_code = 'us'
-url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&APPID={APIKEY}'
+APIKEY = "22499eddd60c8b6d76550fa01c151bcc"
+city = "Wellesley"
+country_code = "us"
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&APPID={APIKEY}"
 
 # print(url)
 f = urllib.request.urlopen(url)
-response_text = f.read().decode('utf-8')
+response_text = f.read().decode("utf-8")
 response_data = json.loads(response_text)
 print(response_data)
 
