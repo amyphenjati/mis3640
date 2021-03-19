85f9bd267ad4aafcc122cd91e9ef8c984bc2488d
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-03-20 00:18:27 +0700

    session18

--- helloflask/app.py
+++ helloflask/app.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'hello, world!'
+@app.route('/hello/<name>')
+def hello(name=None):
+    if name is not None:
+        # return f'<h1> hello, {name}!</h1><p>I am excited to learn Flask</p>'
+        return render_template("hello.html", name=name)
+    return 'Hello, world!'
+
+
+# # make a route like 'square/<number>' so the webapp will display the sqare of the number
+
+@app.route('/square/<number>')
+def square(number):
+    output = float(number) ** 2
+    return f'the square of {number} is {output}'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
