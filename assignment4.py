a6a57e0e8c7b952b78ef9124d2b43cb37cb260f8
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-04-03 00:25:19 +0700

    Create assignment4.py

---
+++ assignment4.py
@@ -0,0 +1,9 @@
+# numpy.percentile()
+
+import numpy
+
+# array
+a = [202, 12, 16, 654, 58]
+print("array : ", a)
+print("50th percentile of arr : ", numpy.percentile(a, 50))
+print("25th percentile of arr : ", numpy.percentile(a, 25))
