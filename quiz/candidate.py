22d05df41f80320580120de535860810517a4391
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-04-07 07:56:40 +0700

    Update candidate.py

--- quiz/candidate.py
+++ quiz/candidate.py
@@ -1,3 +1,30 @@
+#################################
+# Instruction
+# This quiz is a simplified version of 2020 presidential election. Write a definition for a class named Candidate with the following methods:
+
+# An __init__ method that initializes name, winning_states and number of votes.
+# A __str__ method that returns a string representation of this candidate, including name and winning state(s).
+# A method named win_state that takes a string of state abbreviation, adds it to winning_states and updates votes.
+# A special method that overloads the operator > to compare votes of two candidates.
+# #################################
+
+#################################
+# Expected Results
+# Before voting:
+# Donald Trump has won TX, FL.
+# Joe Biden has won CA, MA.
+# Kanye West has not won any state yet.
+# Does Trump win?
+# True
+
+# After election day:
+# Donald Trump has won TX, FL, OH, IA.
+# Joe Biden has won CA, MA, PA, AZ.
+# Does Trump win?
+# False
+#######################################
+
+
 ELECTORAL_VOTES = {'AZ': 11, 'CA': 55, 'FL': 29, 'IA': 6, 'MA': 11, 'OH': 18, 'PA': 20, 'TX': 38}
 
 
@@ -10,7 +37,12 @@ class Candidate:
         winning_states: a list of strings representing initial winning state(s) (even before voting).
         votes: integer, representing number of votes
         """
-        pass
+        if winning_states is None: 
+            winning_states=[]
+        self.winning_states = winning_states
+        self.name = name 
+        self.win_state = winning_states
+        self.votes = votes
 
     def __str__(self):
         """Return a string representation of this candidate,
