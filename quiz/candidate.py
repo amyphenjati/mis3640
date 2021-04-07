199e2250493b87b5cb807a00f77d52d8ad5d5bf9
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-04-07 11:07:50 +0700

    Update candidate.py

--- quiz/candidate.py
+++ quiz/candidate.py
@@ -24,64 +24,86 @@
 # False
 #######################################
 
-
-ELECTORAL_VOTES = {'AZ': 11, 'CA': 55, 'FL': 29, 'IA': 6, 'MA': 11, 'OH': 18, 'PA': 20, 'TX': 38}
+ELECTORAL_VOTES = {
+    "AZ": 11,
+    "CA": 55,
+    "FL": 29,
+    "IA": 6,
+    "MA": 11,
+    "OH": 18,
+    "PA": 20,
+    "TX": 38,
+}
 
 
 class Candidate:
     """The presidential candidate"""
 
+    # An __init__ method that initializes name, winning_states and number of votes.
     def __init__(self, name, winning_states=None, votes=0):
         """Initialize candidate.
         name: string
         winning_states: a list of strings representing initial winning state(s) (even before voting).
         votes: integer, representing number of votes
         """
-        if winning_states is None: 
-            winning_states=[]
-        self.winning_states = winning_states
-        self.name = name 
-        self.win_state = winning_states
+        self.name = name
+        if winning_states is None:
+            self.winning_states = []
+        else:
+            self.winning_states = winning_states
         self.votes = votes
 
+    # A __str__ method that returns a string representation of this candidate, including name and winning state(s).
     def __str__(self):
         """Return a string representation of this candidate,
         including name and winning state(s).
         """
-        pass
-
+        if len(self.winning_states) > 0:
+            return f"{self.name} has won {', '.join(self.winning_states)}."
+        else:
+            return f"{self.name} has not won any state yet."
+            
+    # A method named win_state that takes a string of state abbreviation, adds it to winning_states and updates votes.
     def win_state(self, state):
         """Adds a state to winning_states and updates votes.
         state: a string of state abbreviation
         """
-        pass
+        vote_count = ELECTORAL_VOTES
+        self.winning_states.append(state)
+        self.votes += vote_count[state]
 
+    # A special method that overloads the operator > to compare votes of two candidates.
+    def __gt__(self, other):  #  __gt__ is a method for implementation of >
+        if self.votes > other.votes:
+            return True
+        else:
+            return False
 
 
 ###########################################
 # Please DO NOT change code in main method!
 ###########################################
 def main():
-    trump = Candidate('Donald Trump', winning_states=['TX', 'FL'])
-    biden = Candidate('Joe Biden', winning_states=['CA', 'MA'])
-    west = Candidate('Kanye West')
-    print('Before voting:')
+    trump = Candidate("Donald Trump", winning_states=["TX", "FL"])
+    biden = Candidate("Joe Biden", winning_states=["CA", "MA"])
+    west = Candidate("Kanye West")
+    print("Before voting:")
     print(trump)
     print(biden)
     print(west)
-    print('Does Trump win?')
+    print("Does Trump win?")
     print(trump > biden)
     print()
-    print('After election day:')
-    trump.win_state('OH')
-    trump.win_state('IA')
-    biden.win_state('PA')
-    biden.win_state('AZ')
+    print("After election day:")
+    trump.win_state("OH")
+    trump.win_state("IA")
+    biden.win_state("PA")
+    biden.win_state("AZ")
     print(trump)
     print(biden)
-    print('Does Trump win?')
+    print("Does Trump win?")
     print(trump > biden)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
